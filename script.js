5bd1ad33fb2c4ba6fc6b67620c5b6bbf710d0b20
Driftedbucket <ndinowix@gmail.com>
2025-09-30 05:19:47 +0200

    Defined our refernces for our different html tags

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("registrationForm");
+const cardsContainer = document.getElementById("cardsContainer");
+const tableBody = document.querySelector("#summaryTable tbody");
+const toggleThemeBtn = document.getElementById("toggleTheme");
